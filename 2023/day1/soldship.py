b4471a0829e0f98526597db4a62a7cc538a3a4a6
leafers58 <109424116+leafers58@users.noreply.github.com>
2023-12-01 14:11:55 -0500

    Hi!

--- 2023/day1/soldship.py
+++ 2023/day1/soldship.py
@@ -1,2 +1,12 @@
+import random
+isaac = ["cool", "not cool", "sort of cool"]
 for i in range(6):
     print("soldship is the best lol")
+    cool_factor = random.choice(isaac)
+
+    if cool_factor == "cool":
+        print("Isaac is cool")
+    elif cool_factor == "uncool":
+        print("isaac isn't cool.")
+    else:
+        print("Isaac is sort of cool.")
