acbe3edd063aa0c57e085e5b5f6829579bc8c9e7
Pixelz22 <tljohnson1225@gmail.com>
2023-12-13 14:52:10 -0500

    solved day 13 p1, attempting p2

--- 2023/day13/tyler.py
+++ 2023/day13/tyler.py
@@ -17,32 +17,37 @@ def part_one():
     for pattern in patterns:
         refl_idx = 0
         refl_dist = 0
-        for i in range(1, len(pattern)):
+        i = 1
+        while i < len(pattern):
             candidate_idx = i - refl_dist * 2 - 1
             if candidate_idx < 0:
                 break
             if pattern[i] == pattern[candidate_idx]:
                 refl_idx = i - refl_dist
                 refl_dist += 1
-            else:
+            elif refl_dist != 0:
                 refl_dist = 0
+                i = refl_idx
+            i += 1
 
         if refl_dist > 0:
             summary += 100 * refl_idx  # Vertical reflection
 
         refl_idx = 0
         refl_dist = 0
-        for i in range(1, len(pattern[0])):
+        columns = [''.join([row[i] for row in pattern]) for i in range(len(pattern[0]))]
+        i = 1
+        while i < len(pattern[0]):
             candidate_idx = i - refl_dist * 2 - 1
             if candidate_idx < 0:
                 break
-            column = [row[i] for row in pattern]
-            candidate = [row[candidate_idx] for row in pattern]
-            if column == candidate:
+            if columns[i] == columns[candidate_idx]:
                 refl_idx = i - refl_dist
                 refl_dist += 1
-            else:
+            elif refl_dist != 0:
                 refl_dist = 0
+                i = refl_idx
+            i += 1
 
         if refl_dist > 0:
             summary += refl_idx  # Horizontal reflection
@@ -50,10 +55,84 @@ def part_one():
     print(summary)
 
 
+def line_diff(line1: str, line2: str):
+    diff = 0
+    for i in range(len(line1)):
+        if line1[i] != line2[i]:
+            diff += 1
+    return diff
+
+
 def part_two():
-    file = open("input.txt")
+    file = open("inputs/tylerinput.txt")
+    patterns = []
+    working = []
+    for line in file:
+        line = line.removesuffix('\n')
+        if line == '':
+            patterns.append(working)
+            working = []
+            continue
+
+        working.append(line)
+
+    patterns.append(working)
+
+    summary = 0
+    for pattern in patterns:
+        refl_idx = 0
+        refl_dist = 0
+        i = 1
+        used_smudge = False
+        while i < len(pattern):
+            candidate_idx = i - refl_dist * 2 - 1
+            if candidate_idx < 0:
+                break
+
+            diff = line_diff(pattern[i], pattern[candidate_idx])
+            if diff == 0 or (diff == 1 and not used_smudge):
+                refl_idx = i - refl_dist
+                refl_dist += 1
+                if diff == 1:
+                    used_smudge = True
+            elif refl_dist != 0:
+                refl_dist = 0
+                i = refl_idx
+                used_smudge = False
+            i += 1
+
+        if refl_dist > 0:
+            summary += 100 * refl_idx  # Vertical reflection
+            continue  # No need to look for horizontal reflections
+
+        refl_idx = 0
+        refl_dist = 0
+        columns = [''.join([row[i] for row in pattern]) for i in range(len(pattern[0]))]
+        i = 1
+        used_smudge = False
+        while i < len(pattern[0]):
+            candidate_idx = i - refl_dist * 2 - 1
+            if candidate_idx < 0:
+                break
+
+            diff = line_diff(columns[i], columns[candidate_idx])
+            if diff == 0 or (diff == 1 and not used_smudge):
+                refl_idx = i - refl_dist
+                refl_dist += 1
+                if diff == 1:
+                    used_smudge = True
+            elif refl_dist != 0:
+                refl_dist = 0
+                i = refl_idx
+                used_smudge = False
+            i += 1
+
+        if refl_dist > 0:
+            summary += refl_idx  # Horizontal reflection
+
+    print(summary)
 
 
 if __name__ == "__main__":
-    part_one()
-    # part_two()
+    # part_one()
+    part_two()
