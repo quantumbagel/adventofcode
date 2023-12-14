e2d8f26103f13dd1843cb84c84964875b0a9c135
Pixelz22 <tljohnson1225@gmail.com>
2023-12-13 21:20:48 -0500

    solved day13 p2

--- 2023/day13/tyler.py
+++ 2023/day13/tyler.py
@@ -55,12 +55,55 @@ def part_one():
     print(summary)
 
 
-def line_diff(line1: str, line2: str):
-    diff = 0
-    for i in range(len(line1)):
-        if line1[i] != line2[i]:
-            diff += 1
-    return diff
+def find_mirror_idx(l: list[str], allow_smudge=False) -> int:
+    refl_idx = 0
+    refl_dist = 0
+    i = 1
+    used_smudge = not allow_smudge
+    while i < len(l):
+        candidate_idx = i - refl_dist * 2 - 1  # This is the line we're supposedly mirroring
+
+        # If if we've already compared all existing lines to the left of the mirror,
+        # then we might be done.
+        if candidate_idx < 0:
+            # If we didn't use a smudge when we were supposed to,
+            # go back and find another reflection line.
+            if allow_smudge and not used_smudge:
+                refl_dist = 0
+                i = refl_idx + 1
+                continue
+            break
+
+        # Compare the current and candidate lines.
+        # If we're using smudges, allow for one non-match.
+        is_mirror = True
+        for j in range(len(l[0])):
+            if l[i][j] != l[candidate_idx][j]:
+                if used_smudge:
+                    used_smudge = not allow_smudge
+                    is_mirror = False
+                    break
+                used_smudge = True
+
+        # If the mirror was successful, increase the refl_distance
+        if is_mirror:
+            refl_idx = i - refl_dist
+            refl_dist += 1
+        elif refl_dist != 0:  # If we were working on a reflection
+            refl_dist = 0  # Clear the reflection
+            i = refl_idx  # Go back to where the reflection started so we can check for different ones
+            used_smudge = not allow_smudge  # reset smudge flag
+        i += 1
+
+        # If we didn't use a smudge when we were supposed to,
+        # go back and find another reflection line.
+        if i >= len(l) and refl_dist != 0 and allow_smudge and not used_smudge:
+            refl_dist = 0
+            i = refl_idx + 1
+
+    if refl_dist > 0:  # if we found a reflection
+        return refl_idx
+    return 0
 
 
 def part_two():
@@ -75,60 +118,20 @@ def part_two():
             continue
 
         working.append(line)
-
     patterns.append(working)
 
     summary = 0
     for pattern in patterns:
-        refl_idx = 0
-        refl_dist = 0
-        i = 1
-        used_smudge = False
-        while i < len(pattern):
-            candidate_idx = i - refl_dist * 2 - 1
-            if candidate_idx < 0:
-                break
-
-            diff = line_diff(pattern[i], pattern[candidate_idx])
-            if diff == 0 or (diff == 1 and not used_smudge):
-                refl_idx = i - refl_dist
-                refl_dist += 1
-                if diff == 1:
-                    used_smudge = True
-            elif refl_dist != 0:
-                refl_dist = 0
-                i = refl_idx
-                used_smudge = False
-            i += 1
-
-        if refl_dist > 0:
-            summary += 100 * refl_idx  # Vertical reflection
+        vertical_refl = find_mirror_idx(pattern, True)
+        if vertical_refl > 0:
+            summary += 100 * vertical_refl  # Vertical reflection
             continue  # No need to look for horizontal reflections
 
-        refl_idx = 0
-        refl_dist = 0
         columns = [''.join([row[i] for row in pattern]) for i in range(len(pattern[0]))]
-        i = 1
-        used_smudge = False
-        while i < len(pattern[0]):
-            candidate_idx = i - refl_dist * 2 - 1
-            if candidate_idx < 0:
-                break
-
-            diff = line_diff(columns[i], columns[candidate_idx])
-            if diff == 0 or (diff == 1 and not used_smudge):
-                refl_idx = i - refl_dist
-                refl_dist += 1
-                if diff == 1:
-                    used_smudge = True
-            elif refl_dist != 0:
-                refl_dist = 0
-                i = refl_idx
-                used_smudge = False
-            i += 1
-
-        if refl_dist > 0:
-            summary += refl_idx  # Horizontal reflection
+        horizontal_refl = find_mirror_idx(columns, True)
+        if horizontal_refl > 0:
+            summary += horizontal_refl  # Horizontal reflection
+            continue
 
     print(summary)
 
